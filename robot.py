0cb72dce60699d12cb6cbf22c5ebed73262e25d5
Creedo11 <keegkree@gmail.com>
2022-06-20 23:30:26 -0400

    used the UML to setup all class framework

---
+++ robot.py
@@ -0,0 +1,13 @@
+
+from xml.dom.domreg import well_known_implementations
+
+
+class Robot:
+
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+        self.active_weapon = Weapon
+
+
+    def attack(self, dinosaur):
